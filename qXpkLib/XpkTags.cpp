00cc478584ee4d622e03023eff32f837a1ed36de
Ilkka Prusi <ilkka.prusi@gmail.com>
2011-09-19 04:28:16 +0300

    debugging, testing, does not seem to find any tags so either offset is wrong of tag id defines/handling has bugs.. trying to get rid of them later anyway so should drop them and replace with better choice

--- qXpkLib/XpkTags.cpp
+++ qXpkLib/XpkTags.cpp
@@ -1,6 +1,113 @@
 #include "XpkTags.h"
 
 
+// determine size of data following tag id
+// so we can skip/use it..
+//
+// struct-based has too many problems porting
+// to 32/64 bit cpus so avoid them..
+//
+size_t XpkTags::getChunkSize(uint32_t tagID) const
+{
+	size_t nSize = 0;
+	
+	// note: some of these might not be stored..
+	// no way to know yet..
+	switch (tagID)
+	{
+	case XPK_InName:
+		break;
+	case XPK_InFH:
+		break;
+	case XPK_InBuf:
+		break;
+	case XPK_InHook:
+		break;
+	case XPK_OutName:
+		break;
+	case XPK_OutFH:
+		break;
+	case XPK_OutBuf:
+		break;
+	case XPK_GetOutBuf:
+		break;
+	case XPK_OutHook:
+		break;
+	case XPK_InLen:
+		break;
+	case XPK_OutBufLen:
+		break;
+	case XPK_GetOutLen:
+		break;
+	case XPK_GetOutBufLen:
+		break;
+	case XPK_Password:
+		break;
+	case XPK_GetError:
+		break;
+	case XPK_OutMemType:
+		break;
+	case XPK_PassThru:
+		break;
+	case XPK_StepDown:
+		break;
+	case XPK_ChunkHook:
+		break;
+	case XPK_PackMethod:
+		break;
+	case XPK_ChunkSize:
+		break;
+	case XPK_PackMode:
+		break;
+	case XPK_NoClobber:
+		break;
+	case XPK_Ignore:
+		break;
+	case XPK_TaskPri:
+		break;
+	case XPK_FileName:
+		break;
+	case XPK_ShortError: // obsolete? will we find this in old files?
+		break;
+	case XPK_PackersQuery:
+		// temp!
+		nSize = sizeof(XpkPackerList);
+		break;
+	case XPK_PackerQuery:
+		// temp!
+		nSize = sizeof(XpkPackerInfo);
+		break;
+	case XPK_ModeQuery:
+		// temp!
+		nSize = sizeof(XpkMode);
+		break;
+	case XPK_LossyOK:
+		break;
+	case XPK_NoCRC:
+		break;
+	case XPK_Key16:
+		break;
+	case XPK_Key32:
+		break;
+	case XPK_NeedSeek:
+		break;
+	case XPK_UseXfdMaster:
+		break;
+	case XPK_UseExternals:
+		break;
+	case XPK_PassRequest:
+		break;
+	case XPK_Preferences:
+		break;
+	case XPK_ChunkReport:
+		break;
+	case XPK_MARGIN:
+		break;
+	}
+	return nSize;
+}
+
+
 //////////////////
 //
 // XPK fileformat:
@@ -55,6 +162,11 @@ void XpkTags::ReadStreamHeader(CReadBuffer &Buffer)
 
 /////////////
 // these may be tricky to convert..
+//
+// old stuff uses tag ID to decide type of struct there is
+// and they won't compile correctly on 32/64 bit cpus..
+// get rid of them entirely instead of hacking something together
+// and replace with something more easily portable.
 //
 XpkTag *XpkTags::NextTag(CReadBuffer &Buffer, XpkTag *pPrevious)
 {
@@ -117,7 +229,60 @@ XpkTag *XpkTags::NextTag(CReadBuffer &Buffer, XpkTag *pPrevious)
 	pCurrent->m_Item.ti_Tag = (uint32_t)GetULong(Buffer.GetNext(4)); // tag ID
 	pCurrent->m_Item.ti_Data = (void*)Buffer.GetAtCurrent(); // data of tag in buffer
 	
+	// TEST
+	//pCurrent->m_Item.ti_Tag = Swap4(pCurrent->m_Item.ti_Tag);
+	
 	return pCurrent;
+}
+
+bool XpkTags::ReadTagData(CReadBuffer &Buffer, XpkTag *pTag)
+{
+	// temp!
+	size_t nSize = getChunkSize(pTag->m_Item.ti_Tag);
+	
+	//// TEST!!
+	// temp!
+	switch (pTag->m_Item.ti_Tag)
+	{
+	case XPK_PackersQuery:
+		{
+		XpkPackerList *pMode = (XpkPackerList*)pTag->m_Item.ti_Tag;
+		}
+		break;
+	case XPK_PackerQuery:
+		{
+		XpkPackerInfo *pMode = (XpkPackerInfo*)pTag->m_Item.ti_Tag;
+		}
+		break;
+	case XPK_ModeQuery:
+		{
+		XpkMode *pMode = (XpkMode*)pTag->m_Item.ti_Tag;
+		}
+		break;
+	case XPK_PackMethod:
+		{
+		unsigned int id = idfromname((char*)pTag->m_Item.ti_Tag);
+		}
+		break;
+	case XPK_PackMode:
+		{
+		unsigned int *packmode = (unsigned int*)(pTag->m_Item.ti_Data);
+		}
+		break;
+	case XPK_Preferences:
+		{
+		unsigned int *pPref = (unsigned int*)(pTag->m_Item.ti_Data);
+		}
+		break;
+	}
+	if (nSize > 0)
+	{
+		//pTag->m_pData = new uint8_t[nSize];
+		//::memcpy(pTag->m_pData, Buffer.GetAtCurrent(), nSize);
+		Buffer.SetCurrentPos(Buffer.GetCurrentPos() + nSize);
+	}
+	
+	return false;
 }
 
 void XpkTags::ParseTags(CReadBuffer &Buffer)
@@ -130,6 +295,8 @@ void XpkTags::ParseTags(CReadBuffer &Buffer)
 		{
 			break;
 		}
+		
+		ReadTagData(Buffer, pNext);
 		if (pCurrent == nullptr)
 		{
 			m_pFirstTag = pNext;
--- qXpkLib/XpkTags.h
+++ qXpkLib/XpkTags.h
@@ -4,8 +4,10 @@
 #include <stdint.h>
 #include <limits.h>
 
+// for buffer-handler..
 #include "AnsiFile.h"
 
+/*
 #ifndef UBYTE
 typedef uint8_t UBYTE;
 #endif
@@ -18,23 +20,40 @@ typedef uint16_t UWORD;
 //typedef uint32_t ULONG; <- better but windef.h has with different type..
 typedef unsigned long ULONG;
 #endif
-
+*/
 
 /* constants for Tag.ti_Tag, control tag values */
 /* terminates array of TagItems. ti_Data unused */
+/*
 enum TagType
 {
 	TAG_DONE    = 0,
-	TAG_END	    = 0, /* synonym for TAG_DONE			  */
-	TAG_IGNORE  = 1, /* ignore this item, not end of array		  */
-	TAG_MORE    = 2, /* ti_Data is pointer to another array of TagItems. note that this tag terminates the current array */
+	TAG_END	    = 0, // synonym for TAG_DONE			  
+	TAG_IGNORE  = 1, // ignore this item, not end of array		  
+	TAG_MORE    = 2, // ti_Data is pointer to another array of TagItems. note that this tag terminates the current array 
 
-	TAG_SKIP    = 3, /* skip this and the next ti_Data items	 */
+	TAG_SKIP    = 3, // skip this and the next ti_Data items	 
 
-	/* differentiates user tags from control tags */
+	// differentiates user tags from control tags 
 	TAG_USER    = 1L<<31
-
 };
+*/
+
+/* constants for Tag.ti_Tag, control tag values */
+/* terminates array of TagItems. ti_Data unused */
+#define TAG_DONE   (0L)
+/* synonym for TAG_DONE			  */
+#define TAG_END	   (0L)
+/* ignore this item, not end of array		  */
+#define	TAG_IGNORE (1L)
+/* ti_Data is pointer to another array of TagItems
+ * note that this tag terminates the current array */
+#define	TAG_MORE   (2L)
+/* skip this and the next ti_Data items	 */
+#define	TAG_SKIP   (3L)
+
+/* differentiates user tags from control tags */
+#define TAG_USER   ((unsigned int)(1L<<31))
 
 // note: can't cast directly to this
 // since ptr is 8 bytes on 64-bit build..
@@ -53,14 +72,24 @@ class XpkTag
 {
 public:
 	TagItem m_Item;
+	//void *m_pData;
+	
 	XpkTag *m_pNext;
 	
 public:
 	XpkTag(void)
 	    : m_pNext(nullptr)
+		//, m_pData(nullptr)
 	{}
 	~XpkTag(void)
-	{}
+	{
+		/*
+		if (m_pData != nullptr)
+		{
+			delete m_pData;
+		}
+		*/
+	}
 };
 
 
@@ -77,8 +106,22 @@ public:
 /* Caller must supply ONE of these to tell Xpk#?ack where to get data from */
 /* Process an entire named file */
 #define XPK_InName	  XTAG(0x01)
+/* File handle - start from curposition
+ * If packing partial file, must also supply InLen */
+#define XPK_InFH	  XTAG(0x02)
+/* Single unblocked memory buffer, must also supply InLen */
+#define XPK_InBuf	  XTAG(0x03)
+/* Call custom Hook to read data
+ * Must also supply InLen, when hook cannot do! (not for XPK unpacking) */
+#define XPK_InHook	  XTAG(0x04)
 
 /* Caller must supply 1 of these to tell Xpk#?ackFile where to send data to */
+/* Write (or overwrite) this data file */
+#define XPK_OutName	  XTAG(0x10)
+/* File handle - write from curpos on */
+#define XPK_OutFH	  XTAG(0x11)
+/* Unblocked buf - must also supply OutBufLen */
+#define XPK_OutBuf	  XTAG(0x12)
 /* Master allocs OutBuf - ti_Data points to buf ptr */
 #define XPK_GetOutBuf	  XTAG(0x13)
  /* Callback Hook to get output buffers */
@@ -87,6 +130,50 @@ public:
 /* Other tags for Pack/Unpack */
 /* Length of data in input buffer */
 #define XPK_InLen	  XTAG(0x20)
+/* Length of output buffer     */
+#define XPK_OutBufLen	  XTAG(0x21)
+/* ti_Data pts to long to rec OutLen */
+#define XPK_GetOutLen	  XTAG(0x22)
+/* ti_Data pts to long to rec OutBufLen */
+#define XPK_GetOutBufLen  XTAG(0x23)
+/* Password for de/encoding    */
+#define XPK_Password	  XTAG(0x24)
+/* ti_Data points to buffer for errmsg */
+#define XPK_GetError	  XTAG(0x25)
+/* Memory type for output buffer */
+#define XPK_OutMemType	  XTAG(0x26)
+/* Bool: Passthru unrec. fmts on unpack */
+#define XPK_PassThru	  XTAG(0x27)
+/* Bool: Step down packmethod if nec. */
+#define XPK_StepDown	  XTAG(0x28)
+/* Call this Hook between chunks */
+#define XPK_ChunkHook	  XTAG(0x29)
+/* Do a FindMethod before packing */
+#define XPK_PackMethod	  XTAG(0x2a)
+/* Chunk size to try to pack with */
+#define XPK_ChunkSize	  XTAG(0x2b)
+/* Packing mode for sublib to use */
+#define XPK_PackMode	  XTAG(0x2c)
+/* Don't overwrite existing files */
+#define XPK_NoClobber	  XTAG(0x2d)
+/* Skip this tag               */
+#define XPK_Ignore	  XTAG(0x2e)
+/* Change priority for (un)packing */
+#define XPK_TaskPri	  XTAG(0x2f)
+/* File name for progress report */
+#define XPK_FileName	  XTAG(0x30)
+/* !!! obsolete !!!            */
+#define XPK_ShortError	  XTAG(0x31)
+/* Query available packers     */
+#define XPK_PackersQuery  XTAG(0x32)
+/* Query properties of a packer */
+#define XPK_PackerQuery	  XTAG(0x33)
+/* Query properties of packmode */
+#define XPK_ModeQuery	  XTAG(0x34)
+/* Lossy packing permitted? (FALSE)*/
+#define XPK_LossyOK	  XTAG(0x35)
+/* Ignore checksum             */
+#define XPK_NoCRC         XTAG(0x36)
 /* tags added for xfdmaster support (version 4 revision 25) */
 /* 16 bit key (unpack only)	*/
 #define XPK_Key16	  XTAG(0x37)
@@ -102,10 +189,67 @@ public:
 #define XPK_UseXfdMaster  XTAG(0x40)
 /* Use packers in extern dir (TRUE)*/
 #define XPK_UseExternals  XTAG(0x41)
+/* automatic password req.? (FALSE)*/
+#define XPK_PassRequest   XTAG(0x42)
+/* use prefs semaphore ? (TRUE) */
+#define XPK_Preferences   XTAG(0x43)
+/* automatic chunk report ? (FALSE)*/
+#define XPK_ChunkReport	  XTAG(0x44)
 
 /* tags XTAG(0x50) to XTAG(0x6F) are for XpkPassRequest -- see below */
 #define XPK_MARGIN	256	/* Safety margin for output buffer	*/
 
+#pragma pack(push, 1)
+
+// temp
+#define MAXPACKERS 100
+
+struct XpkPackerList {
+  unsigned int xpl_NumPackers;
+/* NOTE: This isn't 100% compatible with the Amiga version! */
+  unsigned int xpl_Packer[MAXPACKERS];
+};
+struct XpkMode {
+  /* Chain to next descriptor for ModeDesc list*/
+  struct XpkMode *xm_Next;
+  /* Maximum efficiency handled by this mode */
+  unsigned int xm_Upto;
+  /* Defined below                         */
+  unsigned int xm_Flags;
+  /* Extra memory required during packing  */
+  unsigned int xm_PackMemory;
+  /* Extra memory during unpacking         */
+  unsigned int xm_UnpackMemory;
+  /* Approx packing speed in K per second  */
+  unsigned int xm_PackSpeed;
+  /* Approx unpacking speed in K per second */
+  unsigned int xm_UnpackSpeed;
+  /* CF in 0.1%				 */
+  unsigned short xm_Ratio;
+  /* Desired chunk size in K (!!) for this mode*/
+  unsigned short xm_ChunkSize;
+  /* 7 character mode description     */
+  char  xm_Description[10];
+};
+
+struct XpkPackerInfo {
+  /* Brief name of the packer      */
+  char  xpi_Name[24];
+  /* Full name of the packer       */
+  char  xpi_LongName[32];
+  /* One line description of packer */
+  char  xpi_Description[80];
+  /* Defined below                 */
+  unsigned int xpi_Flags;
+  /* Max input chunk size for packing */
+  unsigned int xpi_MaxChunk;
+  /* Default packing chunk size    */
+  unsigned int xpi_DefChunk;
+  /* Default mode on 0..100 scale  */
+  unsigned short xpi_DefMode;
+};
+
+#pragma pack(pop)
 
 // just for memory.. see how to replace..
 /* These structures define the file format for compressed streams */
@@ -141,7 +285,7 @@ struct XpkChunkHdrLong
   ULONG xchl_ULen;
 };
 
-// TODO: does this align correctly in x86?
+// TODO: does this align correctly in x86/x64? (32/64 bit cpu??)
 //
 typedef union 
 {
@@ -205,7 +349,7 @@ struct XpkFileInfo
 	uint32_t m_ID; // IFF-style ID (off: 0)
 	uint32_t m_FileLen; // IFF-style length (off: 4)
 	uint32_t m_ChunkID; // IFF-style ID of cruncher (off: 8)
-	uint32_t m_ChunkLen; // first chunk length (off: 12)
+	uint32_t m_ChunkLen; // first chunk length (off: 12), or uncompressed length?
 	
 	// First 16 bytes of orig. file 
     char  m_Orig_Head[16];
@@ -232,7 +376,6 @@ public:
 	XpkChunk *m_pPrevious;
 	
 	size_t m_nDataOffset; // offset to data (after chunk header)
-	
 };
 
 
@@ -267,11 +410,46 @@ protected:
 		// debug
 		return ((pBuf[0] << 24) + (pBuf[1] << 16) + (pBuf[2] << 8) + pBuf[3]);
 	}
+
+	uint32_t Swap4(const uint32_t val) const
+	{
+		return (
+				((val & 0x000000FF) << 24) + ((val & 0x0000FF00) <<8) |
+				((val & 0x00FF0000) >> 8) + ((val & 0xFF000000) >>24)
+				);
+	}
+	
+	/* Get ID number from string */
+	static char xpkupper(char c)
+	{
+	  if(c>='a' && c<='z')
+	    c-='a'-'A';
+	
+	  return c;
+	}
+	
+	unsigned int idfromname(char *name)
+	{
+	  unsigned int i, j=0;
+	
+	  for(i=4; i; i--) {
+	    j<<=8;
+	    j+=xpkupper(*(name++));
+	  }
+	
+	  return Swap4(j);
+	}
+	
+
+	// get size of structure following the tag..
+	// this is necessary so we can skip/use it
+	size_t getChunkSize(uint32_t tagID) const;
 	
 	void ReadFileInfo(CReadBuffer &Buffer);
 	void ReadStreamHeader(CReadBuffer &Buffer);
 	
 	XpkTag *NextTag(CReadBuffer &Buffer, XpkTag *pPrevious);
+	bool ReadTagData(CReadBuffer &Buffer, XpkTag *pTag);
 	void ParseTags(CReadBuffer &Buffer);
 	
 public:
