2f90d9608dcc0110639af96fbe440fccc701bf6a
Ilkka Prusi <ilkka.prusi@gmail.com>
2011-10-15 04:21:49 +0300

    some refactoring and cleanup, jumps should be made into methods..

--- xpkSQSH/xpkSQSH.cpp
+++ xpkSQSH/xpkSQSH.cpp
@@ -44,9 +44,9 @@ xpkLibraryBase *GetXpkInstance(void)
 }
 
 
-int bfextu(unsigned char *p,int bo,int bc) 
+int32_t bfextu(unsigned char *p, int32_t bo, int32_t bc) 
 {
-	int r;
+	int32_t r;
 	
 	p += bo / 8;
 	r = *(p++);
@@ -63,14 +63,14 @@ int bfextu(unsigned char *p,int bo,int bc)
 
 //#define bfextu1 ((*(src + d0 / 8) >> (7 - (d0 % 8))) & 1)
 
-inline int bfextu1(unsigned char *src, int d0)
+inline int32_t bfextu1(const unsigned char *src, const int32_t d0)
 {
 	return ((*(src + d0 / 8) >> (7 - (d0 % 8))) & 1);
 }
 
-int bfextu3(unsigned char *p,int bo) 
+int32_t bfextu3(unsigned char *p, int32_t bo) 
 {
-	int r;
+	int32_t r;
 	
 	p += bo / 8;
 	r = *(p++);
@@ -82,9 +82,9 @@ int bfextu3(unsigned char *p,int bo)
 	return r;
 }
 
-int bfexts(unsigned char *p,int bo,int bc) 
+int32_t bfexts(unsigned char *p, int32_t bo, int32_t bc) 
 {
-	int r;
+	int32_t r;
 	
 	p += bo / 8;
 	r = *(p++);
@@ -102,9 +102,11 @@ int bfexts(unsigned char *p,int bo,int bc)
 // or is it expecting marker-byte somewhere?
 void unsqsh(unsigned char *src, unsigned char *dst) 
 {
-	// registers
-	int d0,d1,d2,d3,d4,d5,d6,a2,a5;
+	// registers: force 32-bit
+	int32_t d0,d1,d2,d3,d4,d5,d6,a2,a5;
 	unsigned char *a4,*a6;
+	
+	// 
 	unsigned char a3[] = { 2,3,4,5,6,7,8,0,3,2,4,5,6,7,8,0,4,3,5,2,6,7,8,0,5,4,
 	6,2,3,7,8,0,6,5,7,2,3,4,8,0,7,6,8,2,3,4,5,0,8,7,6,2,3,4,5,0 };
 
@@ -117,54 +119,109 @@ void unsqsh(unsigned char *src, unsigned char *dst)
 	d3 = *(src++);
 	*(dst++) = d3;
 
-l6c6:	if (d1 >= 8) goto l6dc;
-	if (bfextu1(src, d0)) goto l75a;
+l6c6:	
+	if (d1 >= 8) 
+	{
+		goto l6dc;
+	}
+	if (bfextu1(src, d0)) 
+	{
+		goto l75a;
+	}
 	d0 ++;
 	d5 = 0;
 	d6 = 8;
 	goto l734;
 
-l6dc:	if (bfextu1(src, d0)) goto l726;
+l6dc:	
+	if (bfextu1(src, d0)) 
+	{
+		goto l726;
+	}
 	d0 ++;
-	if (! bfextu1(src, d0)) goto l75a;
+	if (! bfextu1(src, d0)) 
+	{
+		goto l75a;
+	}
 	d0 ++;
-	if (bfextu1(src, d0)) goto l6f6;
+	if (bfextu1(src, d0)) 
+	{
+		goto l6f6;
+	}
 	d6 = 2;
 	goto l708;
 
 l6f6:	d0 ++;
-	if (! bfextu1(src, d0)) goto l706;
+	if (! bfextu1(src, d0)) 
+	{
+		goto l706;
+	}
 	d6 = bfextu3(src,d0);
 	d0 += 3;
 	goto l70a;
 
 l706:	d6 = 3;
 l708:	d0 ++;
-l70a:	d6 = *(a3 + (8*a2) + d6 - 17);
-	if (d6 != 8) goto l730;
-l718:	if (d2 < 20) goto l722;
+
+l70a:
+	// only place using table in a3 ?
+	d6 = *(a3 + (8*a2) + d6 - 17);
+	if (d6 != 8) 
+	{
+		goto l730;
+	}
+
+l718:	
+	if (d2 < 20) 
+	{
+		// only place using l722
+		// -> moved here
+		//goto l722;
+		
+		d5 = 0;
+		goto l734;
+	}
 	d5 = 1;
 	goto l732;
 
-l722:	d5 = 0;
+/* // only one place jumping here -> moved there
+l722:	
+	d5 = 0;
 	goto l734;
+*/
 
-l726:	d0 += 1;
+l726:	
+	d0 += 1;
 	d6 = 8;
-	if (d6 == a2) goto l718;
+	if (d6 == a2) 
+	{
+		goto l718;
+	}
 	d6 = a2;
+
 l730:	d5 = 4;
 l732:	d2 += 8;
-l734:	d4 = bfexts(src,d0,d6);
+
+l734:	
+	d4 = bfexts(src,d0,d6);
 	d0 += d6;
 	d3 -= d4;
 	*dst++ = d3;
 	d5--;
-	if (d5 != -1) goto l734;
-	if (d1 == 31) goto l74a;
+	if (d5 != -1) 
+	{
+		goto l734;
+	}
+	if (d1 == 31) 
+	{
+		goto l74a;
+	}
 	d1 += 1;
+
 l74a:	a2 = d6;
-l74c:	d6 = d2;
+
+l74c:	
+	d6 = d2;
 	d6 >>= 3;
 	d2 -= d6;
 	if (dst < a6) 
@@ -174,67 +231,135 @@ l74c:	d6 = d2;
 	// this is end of chunk?
 	return;
 
-l75a:	d0++;
-	if (bfextu1(src, d0)) goto l766;
+// following three labels essentially same except d4 value
+// and conditional jump-label..
+
+l75a:	
+	d0++;
+	if (bfextu1(src, d0)) 
+	{
+		goto l766;
+	}
 	d4 = 2;
 	goto l79e;
 
-l766:	d0++;
-	if (bfextu1(src, d0)) goto l772;
+l766:	
+	d0++;
+	if (bfextu1(src, d0)) 
+	{
+		goto l772;
+	}
 	d4 = 4;
 	goto l79e;
 
-l772:	d0++;
-	if (bfextu1(src, d0)) goto l77e;
+l772:	
+	d0++;
+	if (bfextu1(src, d0)) 
+	{
+		goto l77e;
+	}
 	d4 = 6;
 	goto l79e;
 
-l77e:	d0++;
-	if (bfextu1(src, d0)) goto l792;
+l77e:	
+	d0++;
+	if (bfextu1(src, d0)) 
+	{
+		// only this jumping there -> moved here
+		//goto l792;
+		
+		d0++;
+		d6 = bfextu(src,d0,5);
+		d0 += 5;
+		d4 = 16;
+		goto l7a6;
+	}
 	d0++;
 	d6 = bfextu3(src,d0);
 	d0 += 3;
 	d6 += 8;
 	goto l7a8;
 
-l792:	d0++;
+/* // only one place jumping here -> moved
+l792:	
+	d0++;
 	d6 = bfextu(src,d0,5);
 	d0 += 5;
 	d4 = 16;
 	goto l7a6;
+*/
 
-l79e:	d0++;
+l79e:	
+	d0++;
 	d6 = bfextu1(src, d0);
 	d0 ++;
+
 l7a6:	d6 += d4;
-l7a8:	if (bfextu1(src, d0)) goto l7c4;
+
+l7a8:	
+	if (bfextu1(src, d0)) 
+	{
+		goto l7c4;
+	}
 	d0 ++;
-	if (bfextu1(src, d0)) goto l7bc;
+	if (bfextu1(src, d0)) 
+	{
+		// only this jumping there -> moved here
+		//goto l7bc;
+		
+		d5 = 14;
+		a5 = -0x1100;
+		goto l7ca;
+	}
 	d5 = 8;
 	a5 = 0;
 	goto l7ca;
 
-l7bc:	d5 = 14;
+/* // only one place jumping here -> moved
+l7bc:	
+	d5 = 14;
 	a5 = -0x1100;
 	goto l7ca;
+*/
 
-l7c4:	d5 = 12;
+l7c4:	
+	d5 = 12;
 	a5 = -0x100;
-l7ca:	d0++;
+
+l7ca:	
+	d0++;
 	d4 = bfextu(src,d0,d5);
 	d0 += d5;
 	d6 -= 3;
-	if (d6 < 0) goto l7e0;
-	if (d6 == 0) goto l7da;
+	if (d6 < 0) 
+	{
+		goto l7e0;
+	}
+	if (d6 == 0) 
+	{
+		goto l7da;
+	}
 	d1 -= 1;
-l7da:	d1 -= 1;
-	if (d1 >= 0) goto l7e0;
+
+l7da:	
+	d1 -= 1;
+	if (d1 >= 0) 
+	{
+		goto l7e0;
+	}
 	d1 = 0;
-l7e0:	d6 += 2;
+
+l7e0:	
+	d6 += 2;
 	a4 = -1 + dst + a5 - d4;
-l7ex:	*dst++ = *a4++;
+
+l7ex:	
+	*dst++ = *a4++;
 	d6--;
-	if (d6 != -1) goto l7ex;
+	if (d6 != -1) 
+	{
+		goto l7ex;
+	}
 	d3 = *(--a4);
 	goto l74c;
 }
