162db04a623a9c260183a557c8401609952e3ec9
Ilkka Prusi <ilkka.prusi@gmail.com>
2011-10-30 04:21:00 +0200

    comments clean-up after moving code to sub-libraries

--- qXpkLib/XfdSlave.cpp
+++ qXpkLib/XfdSlave.cpp
@@ -1,24 +1,15 @@
 ///////////////////////////////////
 //
-// XFD-decrunching support:
-// slave-decrunchers for use by XFD-master.
+// XFD-decrunching support,
+// helpers and interface for different decrunchers.
 //
-// (May be used in XpkMaster when XFD-decruncher is needed.)
+// This is base-class only for decrunchers,
+// using same interface helps sharing code
+// and will simplify later if different library-handling is needed
+// (such as static linking all together instead of dynamic runtime linking).
 //
 // Ilkka Prusi
 // ilkka.prusi@gmail.com
-//
-/////////
-//
-// This code is conversion/rewrite from M68k-assembler to C/C++ code for portability.
-//
-// Multiple authors:
-/*
-**        written by Mr. Larmer / Wanted Team        **
-**            fixed by SDI in 2000/2001		     **
-**    small changes by Don Adan/WT in 2000/2001	     **
-**       quick fix by Ronald van Dijk in 2002        **
-*/
 //
 
 #include "XfdSlave.h"
--- qXpkLib/XfdSlave.h
+++ qXpkLib/XfdSlave.h
@@ -1,9 +1,12 @@
 ///////////////////////////////////
 //
-// XFD-decrunching support:
-// slave-decrunchers for use by XFD-master.
+// XFD-decrunching support,
+// helpers and interface for different decrunchers.
 //
-// (May be used in XpkMaster when XFD-decruncher is needed.)
+// This is base-class only for decrunchers,
+// using same interface helps sharing code
+// and will simplify later if different library-handling is needed
+// (such as static linking all together instead of dynamic runtime linking).
 //
 // Ilkka Prusi
 // ilkka.prusi@gmail.com
